b5459544ac679da3a894711d4bc4ea6e4877d9ba
NonnieNguyen <alannguyen514@gmail.com>
2022-10-01 23:51:50 -0400

    Matching algorithm
    find the closest score to the user, does not return anything yet

--- match.js
+++ match.js
@@ -1,11 +1,17 @@
 // import { FirebaseError } from "firebase/app"
 // import {Database, getDatabase, ref} from "firebase/database"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+<<<<<<< Updated upstream
 import {getDatabase, ref, onValue} from "firebase/database";
 
 // Get the respective ID's from the Database
 const auth = getAuth();
 const db = getDatabase();
+=======
+import {makeMap} from extract.js;
+
+const firebase = getAuth();
+>>>>>>> Stashed changes
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -14,6 +20,7 @@ onAuthStateChanged(auth, (user) => {
         getUserScore(user.uid);
         getUserRankScore(user.uid);
     }
+    
 })
 
 
@@ -45,4 +52,24 @@ export function updateScoreRank(uid, score){
     snapshot.ref.update({score: score})
     });
 
+}
+
+// finds the closest score to the user
+function userMatching(uid) {
+    userDict = makeMap();
+    var userScore = getUserScore(uid);
+    var closestUser; // stores the closest scores uid to the original
+    var closestVal = 0;
+    let end = true;
+    let z = 0;
+
+    // iterates through the map until
+    for ([key,val] of Object.entries(userDict)){
+        if (key != uid){
+            if (Math.abs(userScore - val) <= Math.abs(userScore - closestVal)){
+                closestUser = key;
+                closestVal = val;
+            }
+        }        
+    }
 }
